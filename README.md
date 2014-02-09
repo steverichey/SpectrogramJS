3f18a128b3f236b880a86c1f32de3d7310e70002
steverichey <stevenpatrickrichey@gmail.com>
2014-02-08 19:50:19 -0600

    drag and drop works

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-SpectrogramJS
-=============
+# SpectrogramJS
 
-A JavaScript/HTML tool for obfuscating images in the spectrogram of audio files.
+A JavaScript tool for obfuscating images in the spectrogram of audio files. Designed to be simple, free, and fast.
+
+## Project Goals:
+* Drag and drop images into browser.
+* Client-side algorithm to convert image to a lossless sound file.
+* Toggle between linear and logarithmic modes.
+* Preview output before saving sound file.
+* Nifty progress bar.
+
+## Current Status:
+* Basic HTML framework in place.
+
+Copyright (C) 2014 [Steve Richey](http://www.steverichey.com).
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
--- spectrogram.js
+++ spectrogram.js
@@ -1,21 +1,103 @@
-$(document).ready(function() {
-	$('#download').hide();
-	var processcomplete = function () {
-		$('#output').html( "Processing complete..." );
-		$('#download').show();
-	};
-    $('#begin').click(function () {
-        if ( $( "#fileupload" ).val() != "" ) {
-			$('#output').html( "Processing..." );
-			processcomplete();
-		} else {
-			$('#output').html( "Please select a valid image file." );
-		}
-    });
-	$('#dlbutton').click(function () {
-		console.log( "yep" );
-	});
-	var processimage = function() {
-		
-	};
-});
+/*global console*/
+/*global window*/
+/*global document*/
+/*global alert*/
+/*global Image*/
+/*global FileReader*/
+
+/**
+ * A reference to the document canvas.
+ */
+var canvas;
+/**
+ * A reference to the document canvas context.
+ */
+var ctx;
+/**
+ * An image we can use later to draw to the canvas.
+ */
+var image = new Image();
+/**
+ * The maximum height to display the dropped image.
+ */
+var MAX_HEIGHT = 100;
+
+/**
+ * Scales the image size to the maximum height.
+ */
+function setImageSize(thisImage) {
+    'use strict';
+    if (thisImage.height > MAX_HEIGHT) {
+        thisImage.width *= MAX_HEIGHT / image.height;
+        thisImage.height = MAX_HEIGHT;
+    }
+}
+
+/**
+ * Shows the dropped image in the canvas.
+ */
+function render(src) {
+    'use strict';
+    
+    image.src = src;
+	image.onload = setImageSize(image);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	canvas.width = image.width;
+	canvas.height = image.height;
+	ctx.drawImage(image, 0, 0, image.width, image.height);
+}
+
+/**
+ * Render the result of an event.
+ */
+function renderResult(event) {
+    'use strict';
+    render(event.target.result);
+}
+
+/**
+ * Loads an image from a specified source.
+ */
+function loadImage(src) {
+    'use strict';
+    
+    if (!src.type.match(/image*/)) {
+        alert("The dropped file is not an image! It's a " + src.type);
+        return;
+    }
+    
+    var reader = new FileReader();
+    reader.onload = renderResult;
+    reader.readAsDataURL(src);
+}
+
+/**
+ * Simple wrapper for preventDefault.
+ */
+function preventDefault(event) {
+    'use strict';
+    event.preventDefault();
+}
+
+/**
+ * Get the image from a drop event on the canvas.
+ */
+function prepLoadImage(event) {
+    'use strict';
+    preventDefault(event);
+    loadImage(event.dataTransfer.files[0]);
+}
+
+/**
+ * Initialize canvas and context variables.
+ */
+function init() {
+    'use strict';
+    
+    canvas = document.getElementById('spectrogramcanvas');
+    ctx = canvas.getContext('2d');
+    canvas.addEventListener("dragover", preventDefault, true);
+    canvas.addEventListener("drop", prepLoadImage, true);
+}
+
+window.onload = init;
